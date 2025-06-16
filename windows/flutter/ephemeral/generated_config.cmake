d61c7d0045589c12e80ef7dc3d79e4668745dc8c
IgorCafazzi <162507362+IgorCafazzi@users.noreply.github.com>
2025-06-16 18:20:49 -0300

    windows

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Igor\\Downloads\\flutter_windows_3.32.4-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "S:\\androidstudio\\trabalhoalinemobile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Igor\\Downloads\\flutter_windows_3.32.4-stable\\flutter"
+  "PROJECT_DIR=S:\\androidstudio\\trabalhoalinemobile"
+  "FLUTTER_ROOT=C:\\Users\\Igor\\Downloads\\flutter_windows_3.32.4-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=S:\\androidstudio\\trabalhoalinemobile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=S:\\androidstudio\\trabalhoalinemobile"
+  "FLUTTER_TARGET=S:\\androidstudio\\trabalhoalinemobile\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NmZiYTI0NDdlOQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049OGNkMTllNTA5ZA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=S:\\androidstudio\\trabalhoalinemobile\\.dart_tool\\package_config.json"
+)
